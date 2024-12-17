72b824dc19147a1e197042f49f70b3a4fa806f19
Mike Piccolo <mfpiccolo@gmail.com>
2024-12-17 15:11:16 -0600

    working demo

--- playground/src/workflows/policy-approval/components/policy-updater/index.js
+++
@@ -1,7 +0,0 @@
-// index.js boilerplate
-export const subscribe = ["example.my-event"];
-
-export default async function policyUpdaterHandler(input, emit, eventType) {
-  // Add your logic here
-  console.log("Received event:", eventType, input);
-}
