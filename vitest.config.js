6ce7e9cd2f0ba1e2192fb312fdc4aa1c4ea50da8
Mike Piccolo <mfpiccolo@gmail.com>
2024-12-20 14:17:35 -0600

    working test!

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    setupFiles: []
+    environment: "node",
+    setupFiles: [],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/tests/e2e/**", // ensures Vitest doesn't pick up E2E tests
+    ],
   },
 });
