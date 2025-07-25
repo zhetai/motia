f243019b8fc78a10a7bbbb6ec96f843ef093e30b
Anderson Leal <andersonofl@gmail.com>
2025-07-25 09:13:48 -0300

    #471 - Fix to ensure compatibility of postcss.config with Node.js 18+. (#505)

--- packages/workbench/postcss.config.mjs
+++ packages/workbench/postcss.config.mjs
@@ -1,9 +1,13 @@
 import path from 'path'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default {
   plugins: {
-    "@tailwindcss/postcss": {
-      base: path.join(import.meta.dirname, './src'),
+    '@tailwindcss/postcss': {
+      base: path.join(__dirname, './src'),
     },
-  }
+  },
 }
