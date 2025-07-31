62d327066de05043e7490110b57b3b07a94acbc0
Ytallo <ytallo.layon@gmail.com>
2025-07-31 16:56:48 -0300

    Chore/change test viewport (#537)

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -27,12 +27,12 @@ export const config: PlaywrightTestConfig = {
   testIgnore: '**/release/**',
 
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'chromium', use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1080 } } },
     ...(process.env.CI
       ? []
       : [
-          { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-          { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+          { name: 'firefox', use: { ...devices['Desktop Firefox'], viewport: { width: 1920, height: 1080 } } },
+          { name: 'webkit', use: { ...devices['Desktop Safari'], viewport: { width: 1920, height: 1080 } } },
         ]),
   ],
 }
